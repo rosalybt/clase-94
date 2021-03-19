6fba1ff8e4904ae05185b9b92ace69223d13e42f
malerey <reymalena@gmail.com>
2021-03-19 20:40:59 -0300

    clase 94

--- src/components/Tarea.js
+++ src/components/Tarea.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './Tarea.css'
 
-const Tarea = ({ tarea }) => {
+const Tarea = ({ tarea, borrarElementoDelArray }) => {
 
   const [completada, setCompletada] = useState(false)
 
@@ -9,10 +9,17 @@ const Tarea = ({ tarea }) => {
   const handleClick = () => {
     setCompletada(true)
   }
+
+  const handleClickBorrar = () => {
+    borrarElementoDelArray([1, 2, 3])
+  }
+
   return (
     <>
     <li className={completada ? "completada" : ""}>{tarea}</li> 
     <button onClick={handleClick}>Completar tarea</button>
+    <button onClick={handleClickBorrar}>Borrar tarea</button>
+    <button>Modificar tarea</button>
   </>
   )
 }
